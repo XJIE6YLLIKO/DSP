5fd92ada1d190a227a55ac67ed7be3f76369cdfe
A cup <georgiyegoriy@gmail.com>
2025-09-15 16:18:06 +0300

    qwe

--- pz2.m
+++ pz2.m
@@ -6,12 +6,10 @@ data = fread(fid, 'uint8')
 fclose(fid);
 data1="";
 for i=1:length(data)
-  data1=strcat(data1,dec2bin(data(i)));
+  data1=strcat(data1,dec2bin(data(i),8))
 end
-data1
-data1(3)
 
-res=[]
+res=[];
 [y1,fs1]=audioread("sample1.wav");
 [y2,fs2]=audioread("sample2.wav");
 
@@ -25,4 +23,30 @@ end
 
 audiowrite("res.wav", res, fs1)
 
+half_decoded=(xcorr(y1,res)-xcorr(y2,res));
+figure;
+plot(res)
+figure;
+plot((xcorr(y1,res)-xcorr(y2,res)))
 
+figure;
+positives=half_decoded>16.5;
+plot(positives)
+title("positives");
+figure;
+negatives=half_decoded<-16;
+plot(negatives)
+title("negatives");
+
+out_data=[];
+for i=1:length(half_decoded)
+  if positives(i)==1
+    out_data=[out_data;1];
+  end
+  if negatives(i)==1
+    out_data=[out_data;0];
+  end
+end 
+
+out_data=reshape(out_data, 1, [])
+pause
