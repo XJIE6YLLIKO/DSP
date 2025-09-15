e380237f1009d11aa28a8fe5bd06a0a821b8261b
A cup <georgiyegoriy@gmail.com>
2025-09-15 15:45:20 +0300

    octave-based initial commit

--- pz1.m
+++ pz1.m
@@ -1,51 +1,46 @@
-%% Анализ звуков
+pkg load signal
+
 figure(1)
-% subplot(2, 3, 1)
+subplot(2, 3, 1)
 [y1,fs1]=audioread("щелчок.wav");
 y1=y1(1:4000);
-y1=y1*1.4;
-plot(y1)
+y1*=1.4;
+plot(y1, 'Color', 'black')
 xlim([0 length(y1)])
-title("Звук 1")
+title("звук 1")
 grid on
-% length(y1)
-% exportgraphics(gcf, 'audio1.png');
+length(y1)
 
-figure(2)
-% subplot(2, 3, 2)
+subplot(2, 3, 2)
 [y2,fs2]=audioread("вав.wav");
 y2=y2(5001:9000);
-plot(y2)
+plot(y2, 'Color', 'black')
 xlim([0 length(y2)])
-title("Звук 2")
+title("звук 2")
 grid on
-% length(y2)
-% exportgraphics(gcf, 'audio2.png');
+length(y2)
 
-figure(3)
-% subplot(2, 3, 4)
+subplot(2, 3, 4)
 [R1, lags1] = xcorr(y1);
-plot(lags1, R1)
+plot(lags1, R1, 'Color', 'black')
 title("АКФ 1")
 grid on
 xlim([min(lags1) max(lags1)])
-% exportgraphics(gcf, 'AKF1.png');
 
-figure(4)
-% subplot(2, 3, 5)
+subplot(2, 3, 5)
 [R2, lags2] = xcorr(y2);
-plot(lags2, R2)
+plot(lags2, R2, 'Color', 'black')
 title("АКФ 2")
 grid on
 xlim([min(lags2) max(lags2)])
-% exportgraphics(gcf, 'AKF2.png');
 
-figure(5)
-% subplot(2, 3, 6)
+subplot(2, 3, 6)
 [R3, lags3] = xcorr(y1, y2);
-plot(lags3, R3)
-title("ВКФ")
+plot(lags3, R3, 'Color', 'black')
+title("ВКР")
 grid on
 xlim([min(lags3) max(lags3)])
-% exportgraphics(gcf, 'VKF.png');
 
+audiowrite("sample1.wav", y1, fs1);
+audiowrite("sample2.wav", y2, fs2);
+pause
--- pz2.m
+++ pz2.m
@@ -1,21 +1,28 @@
-%% Читаем
-bytes = fileread('text');
-bytes = reshape(dec2bin(double(bytes),8)',1,[]);
+pkg load signal
 
-disp(bytes)
+fid = fopen('text.txt', 'rb');
+data = fread(fid, 'uint8')
+%data = dec2bin(data)
+fclose(fid);
+data1="";
+for i=1:length(data)
+  data1=strcat(data1,dec2bin(data(i)));
+end
+data1
+data1(3)
 
-%% Меняем биты на звук
-
+res=[]
 [y1,fs1]=audioread("sample1.wav");
 [y2,fs2]=audioread("sample2.wav");
 
-out = [];
-
-for i = 1:length(bytes)
-    if bytes(i) == 0
-        out = [out, y1];
-    end
-    out = [out, y2];
+for i=1:length(data1)
+  if data1(i)=="1"
+    res=[res;y1];
+  else
+    res=[res;y2];
+  end
 end
 
-audiowrite('out.wav', out, 44100);
+audiowrite("res.wav", res, fs1)
+
+
