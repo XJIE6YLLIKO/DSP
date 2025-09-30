e447a6cff3461e47ecfe6fdb35575841aaea89e0
acup1 <georgiyegoriy@gmail.com>
2025-09-30 03:18:43 +0300

    qwe

--- pz32.m
+++ pz32.m
@@ -9,21 +9,47 @@ sample_rate=1/((1/fs)*(length(y1)-1))
 half_decoded=(xcorr(y3,y1)-xcorr(y3,y2));
 half_decoded=half_decoded(length(half_decoded)/2:end);
 half_decoded=half_decoded/max(half_decoded);
-half_decoded=half_decoded>0.5;
 figure(1);
 t=0:1/fs:(length(half_decoded)-1)*(1/fs);
 plot(t,half_decoded)
+
+% peaks=zeros(1,length(half_decoded));
+% for i=2:length(peaks)-1
+%   if and(half_decoded(i-1)<half_decoded(i),half_decoded(i)>half_decoded(i+1))
+%     if half_decoded(i)>.5
+%       peaks(i)=-1;
+%     elseif and(.4<half_decoded(i),half_decoded(i)<.5)
+%       peaks(i)=1;
+%     end
+%   end
+% end
+% hold on
+% plot(t,peaks)
+% pause
+
 % hold on
 % plot(t,(round(cos(pi*sample_rate*t)*10)/10==0))
+% half_decoded=half_decoded>0.5;
 
 figure(2);
 f=linspace(-fs/2,fs/2, length(half_decoded));
-sig=zeros(1,length(t));
-for i=1:length(t)
-  sig(i)=half_decoded(i)*cos(2*pi*sample_rate*t(i));
-end
+
+sig=half_decoded;
+% sig=zeros(1,length(t));
+
+% for i=1:length(t)
+%   sig(i)=half_decoded(i)*cos(2*pi*sample_rate*t(i));
+% end
+
 spec=fftshift(fft(sig));
-spec(abs(f)>.5*sample_rate)=0;
+% spec(abs(abs(f)-sample_rate)>.5*sample_rate)=0;
+spec(round(abs(f))!=round(sample_rate))=0;
+
+%spec=spec(abs(f-sample_rate)<=.5*sample_rate);
+%f=linspace(-sample_rate/2,sample_rate/2,length(spec));
+%% f=f(abs(f)<=.5*sample_rate);
+%t=linspace(0,(1/fs)*(length(y3)-1),length(spec));
+
 % spec(abs(f)<0.5*sample_rate)=0;
 
 plot(f,abs(spec))
@@ -33,8 +59,20 @@ sig=real(ifft(ifftshift(spec)));
 figure(3);
 sig=sig/max(sig);
 plot(t,sig)
+hold on
+plot(t,half_decoded)
+
+pause
+% hold on
+% plot(t, -sin(pi*sample_rate*t))
+
+% figure(31);
+% plot(t,unwrap(angle(sig)))
 sig=abs(round(sig/max(sig)));
-% pause
+figure(32);
+plot(t,sig)
+% sprintf('%d', sig)
+pause
 
 decoded=(sig>(max(sig)/2)) .* (cos(2*pi*sample_rate*t)==1)-(sig<=(max(sig)/2)) .* (cos(2*pi*sample_rate*t)==1);
 % decoded=(sig>0) .* (cos(2*pi*sample_rate*t)==1)-(sig<=0) .* (cos(2*pi*2*sample_rate*t)==1);
@@ -62,7 +100,7 @@ end
 out=out(1:end-1);
 out = sprintf('%d', out)
 
-out = reshape(out,8,[])'
+% out = reshape(out,8,[])'
 % length(out)/8
 % char(bin2dec(out))'
 
